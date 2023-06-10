f6cf15d2dfa6808e391283636e4fdc0519226bbe
Suraj <suraj.03062002@gmail.com>
2023-06-10 17:45:33 +0000

    Update 5 files

--- src/components/navbar/navelem/darkmode.jsx
+++ src/components/navbar/navelem/darkmode.jsx
@@ -1,13 +1,11 @@
-import { useColorMode, Button } from "@chakra-ui/react"
-import { color } from "framer-motion"
+import { useColorMode, Button } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
-export default
-function Example() {
-    const { colorMode, toggleColorMode } = useColorMode()
-    return (
-    
-        <Button onClick={toggleColorMode} color={"darkblue"}>
-          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-        </Button>
-    )
-  }
+export default function Example() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Button onClick={toggleColorMode} color={"darkblue"}>
+      Toggle {colorMode === "light" ? "Dark" : "Light"}
+    </Button>
+  );
+}
